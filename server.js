7e28cffadf66c276a02331c10059ec2a1751eed0
Frank Sun <popsun2010@gmail.com>
2015-09-24 17:52:44 -0700

    Initial commit and all features are working

---
+++ server.js
@@ -0,0 +1,20 @@
+// express and app
+var express = require('express');
+var app = express();
+
+// path and bodyParser
+var path = require('path');
+var bodyParser = require('body-parser');
+
+// params over URL
+
+app.use(express.static(path.join(__dirname, "./")));
+// view folder and view engine
+app.use(bodyParser.json());
+
+
+
+// server listen and port number
+app.listen(2010, function(){
+	console.log('Mini Store is on port 2010');
+})
